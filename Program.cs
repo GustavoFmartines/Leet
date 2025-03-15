c0fb5c7ae69c6ed38550b04d886efd16ca5a7869
Gustavo Figueiredo Matines <gustavo.martines2003@outlook.com>
2025-03-14 21:43:05 -0300

    Foi criado o program que conver texto tem Leet

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string texto;
+string textoLeet;
+string textoMaiusculo;
+
+Console.Clear();
+
+
+Console.WriteLine("Escreve um texto: ");
+texto = Console.ReadLine()!;
+textoMaiusculo = texto.ToUpper();
+textoLeet = textoMaiusculo.Replace("A", "4")
+                 .Replace("E", "3")
+                 .Replace("O", "0")
+                 .Replace("I", "1")
+;
+Console.WriteLine($"Texto em Leet: {textoLeet}");
